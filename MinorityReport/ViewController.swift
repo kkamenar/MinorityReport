95b6c131363fc2db08b27d1dce223dbb1abd36ec
Kristin Kamenar <kkamenar@kristins-macbook-air-5.local>
2016-06-16 12:14:55 -0500

    Return future label to center when drag is stopped. Display alert when future label collides with pre cog label.

--- MinorityReport/ViewController.swift
+++ MinorityReport/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var futureLabel: UILabel!
+    @IBOutlet weak var preCogLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onDrag(sender: UIPanGestureRecognizer) {
+        
+        //sends as a CGPoint
+        let point = sender.locationInView(view)
+        futureLabel.center = point
+        
+        //when released, animate back to the center
+        
+        //check to see when dragging stops
+        if sender.state == .Ended
+        {
+            UIView.animateWithDuration(0.75, animations: { 
+                self.futureLabel.center = self.view.center
+            })
+        }
+        
+        else if CGRectContainsPoint(preCogLabel.frame, point)
+        {
+            let myAlert = UIAlertController(title: "Minority Report", message: "Pre Cog Detected", preferredStyle: UIAlertControllerStyle.Alert)
+            //dismiss button
+            myAlert.addAction(UIAlertAction(title: "Arrest Them!", style: .Default, handler: nil))
+            
+            //second button
+            myAlert.addAction(UIAlertAction(title:"Let Them Go!", style: .Default, handler: nil))
+            
+            //show alert view
+            presentViewController(myAlert, animated: true, completion: nil)
+            
+        }
+        
+    }
 
 }
 
